241959f203c52cf9f609c6be5c63e8f1b12f82dd
unknown <danblanfrance@gmail.com>
2022-01-22 13:09:45 +0100

    HW_1 added. Aryphmetic

---
+++ HW1.py
@@ -0,0 +1,121 @@
+import math
+#1. Create an item_1 variable of integer type.
+item_1 = 10
+#2. Create a variable item_2 of integer type.
+item_2 = 3
+#3. Create a variable result_sum in which you sum up item_1 and item_2.
+result_sum = item_1 + item_2
+#4. Output the result_sum to the console.
+print(result_sum)
+#5. Create a variable result_subtr in which you subtract the larger variable from the smaller one.
+result_subtr = item_1 - item_2
+#6. Output result_subtr to the console.
+print(result_subtr)
+#7. Create a result_multi variable in which you multiply item_1 by item_2.
+result_multi = item_1 * item_2
+#8. Output result_multi to the console.
+print(result_multi)
+#9. Create a variable result_exp in which you multiply item_1 by item_2.
+result_exp = item_1 ** item_2
+#10. Output result_exp to the console.
+print(result_exp)
+#11. Create a variable result_m_exp in which you raise item_1 to the power of item_2 using the math library.
+result_m_exp = math.pow (item_1, item_2)
+#12. Output result_m_exp to the console.
+print(result_m_exp)
+#13. Create a variable result_s_root in which you find the square root of any of the item variables
+result_s_root = item_1 ** 0.5
+#14. Output result_s_root to the console.
+print(result_s_root)
+#15. Create a variable result_m_s_root in which you find the square root of any item variable using the math library.
+result_m_s_root = math.sqrt(item_1)
+#16. Output result_m_s_root to the console.
+print(result_m_s_root)
+#17. Create a variable result_mp_s_root in which you find the square root of any item variable using the math library using the pow method.
+result_mp_s_root = math.pow(item_1, 0.5)
+#18. Output result_mp_s_root to the console.
+print(result_mp_s_root)
+#21. Create a variable result_division in which you divide item_1 by item_2.
+result_division = item_1 / item_2
+#22. Output result_division to the console.
+print(result_division)
+#23. Create a variable result_m_floor and round result_division to the nearest integer less than result_division.
+result_m_floor = math.floor(result_division)
+#24. Output result_m_floor to the console.
+print(result_m_floor)
+#25. Create a variable result_m_ceil and result_division rounded to the nearest integer greater than result_division.
+result_m_ceil = math.ceil(result_division)
+#26. Output result_m_ceil to the console.
+print(result_division)
+#27. Create a variable result_int and round result_division to the nearest integer through explicit conversion.
+result_int = round(result_division)
+#28. Output result_int to the console.
+print(result_int)
+#29. Create a variable result_no_division_loss in which you divide item_1 by item_2 without a remainder.
+result_no_division_loss = item_1 // item_2
+#30. Output the result_no_division_loss to the console.
+print(result_no_division_loss)
+#31. Create a variable result_division_loss in which you find the remainder of item_1 divided by item_2.
+result_division_loss = item_1 % item_2
+#32. Print result_division_loss to the console.
+print(result_division_loss)
+
+#33. Create an item_3 variable and assign an integer to it
+item_3 = 8
+#34. Add 10 to item_3 with assignment.
+item_3 += 10
+#35. Output item_3 to the console.
+print(item_3)
+#36. Subtract 5 from item_3 with assignment.
+item_3 -= 5
+#37. Output item_3 to the console.
+print(item_3)
+#38. Multiply item_3 by 3 with assignment.
+item_3 *= 3
+#39. Output item_3 to the console.
+print(item_3)
+#40. Divide item_3 by 2 with assignment.
+item_3 /= 2
+#41. Output item_3 to the console.
+print(item_3)
+#42. Expand item_3 to the power of 2 with assignment
+item_3 **= 2
+#43. Output item_3 to console.
+print(item_3)
+#44. Find the square root of item_3 with assignment.
+item_3 **= 0.5
+#45. Output item_3 to the console.
+print(item_3)
+#46. Assign the remainder of the division of item_3
+item_3 %= 2
+#47. Output item_3 to the console.
+print(item_3)
+#48. Create b_item_t variable and assign it to True.
+b_item_t = True
+#49. Create variable b_item_f and set to False
+b_item_f = False
+#50. Create variable b_item_relult_sum and assign the sum of b_item_t and b_item_f
+b_item_sum = b_item_t + b_item_f
+#51. Output b_item_relult_sum to the console.
+print(b_item_sum)
+#52. Create a b_item_relult_subtr variable and assign the difference of b_item_t and b_item_f
+b_item_result_subtr = b_item_t - b_item_f
+#53. Output b_item_relult_subtr to the console.
+print(b_item_result_subtr)
+#54. Create a b_item_relult_multi variable and assign the multiplication of b_item_t and b_item_f
+b_item_result_multi = b_item_t * b_item_f
+#55. Output b_item_relult_multi to the console.
+print(b_item_result_subtr)
+#56. Create variable b_item_relult_division and assign division to b_item_t and b_item_f
+b_item_result_division = b_item_t / b_item_f
+#57. Output b_item_relult_division to the console. (Get an error.)
+print(b_item_result_division)
+#58. Create variable b_item_1_int and assign an explicit conversion of b_item_t to int
+b_item_1_int = int(b_item_t)
+#59. Output b_item_1_int to the console.
+print(type(b_item_1_int), b_item_1_int)
+#60. Create variable b_item_2_int and assign an explicit conversion of b_item_2 to int
+b_item_2_int = int(b_item_f)
+#61. Output b_item_2_int to the console.
+print(type(b_item_2_int), b_item_2_int)
+
